9716f457b1f289092a0f3c0488629d94038c006f
John Martinsson <john.martinsson@gmail.com>
2021-09-30 16:07:46 +0200

    Change expected output from module_dataset

--- run.py
+++ run.py
@@ -5,10 +5,10 @@
 
 import sys
 import pprint
-import logging
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
-                    level=logging.DEBUG,
-                    stream=sys.stdout)
+#import logging
+#logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+#                    level=logging.DEBUG,
+#                    stream=sys.stdout)
 import numpy as np
 
 import utils
@@ -28,37 +28,29 @@ def main(yaml_filepath, mode):
         module_model         = utils.load_module(cfg['model']['script_path'])
         module_optimizer     = utils.load_module(cfg['optimizer']['script_path'])
         module_loss_function = utils.load_module(cfg['loss_function']['script_path'])
+        module_train         = utils.load_module(cfg['train']['script_path'])
+        #module_evaluate      = utils.load_module(cfg['evaluate']['script_path'])
 
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(cfg)
 
-        x_train, y_train, x_valid, y_valid, x_test, y_test = module_dataset.load_dataset(cfg['dataset'])
-
-        optimizer = module_optimizer.load(cfg['optimizer'])
-        loss_function = module_loss_function.load()
+        train_loader, valid_loader, test_loader = module_dataset.load_dataset(cfg['dataset'])
 
         model = module_model.load(cfg['model'])
+
+        optimizer = module_optimizer.load(model.parameters(), cfg['optimizer'])
+        loss_function = module_loss_function.load(cfg['loss_function'])
+
 
         #if 'initial_weights_path' in cfg['train']:
         #    model.load_weights(cfg['train']['initial_weights_path'])
 
         # training mode
         if mode == 'train':
-            train(model, optimizer, loss_function, x_train, y_train, x_valid, y_valid, cfg)
+            module_train.train(model, optimizer, loss_function, train_loader, valid_loader, cfg['train'])
         # evaluation mode
         if mode == 'evaluate':
-            evaluate(model, x_test, y_test, cfg)
-
-def train(model, optimizer, loss_function, x_train, y_train, x_valid, y_valid, cfg):
-    model.train()
-
-    return model
-
-def evaluate(model, x_test, y_test, cfg):
-    model.eval()
-
-    return
-
+            module_evaluate.evaluate(model, test_loader, cfg['evaluate'])
 
 if __name__ == '__main__':
     args = utils.get_parser().parse_args()
