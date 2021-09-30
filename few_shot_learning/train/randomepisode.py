3c44ec1b154e68043b5e40291094db2cff56a68f
Martin Willbo <martinwillbo@gmail.com>
2021-09-30 14:09:36 +0200

    refactor

---
+++ few_shot_learning/train/randomepisode.py
@@ -0,0 +1,43 @@
+import torch.utils.data as data
+import torch
+import numpy as np
+
+class RandomEpisodicSampler(data.Sampler):
+    
+    #Include the option to choose the number of query samples
+    #Y_train -> labels, just a list of the targets (list of ints?)
+    def __init__(self, labels, n_episodes, n_way, n_support, n_query):
+        
+        #Number of episodes per epoch. len(labels)/(n_support * n_query) ?
+        self.n_episodes = n_episodes
+        self.n_way = n_way
+        self.n_support = n_support
+        self.n_query = n_query
+        self.n_samples = n_support+n_query
+        
+        labels = np.array(labels)
+        self.sample_indices = []
+        for i in range(max(labels) + 1):
+            ix = np.argwhere(labels == i).reshape(-1)
+            ix = torch.from_numpy(ix)
+            self.sample_indices.append(ix)
+            
+        if self.n_way > len(self.sample_indices):
+            #print(self.n_way)
+            raise ValueError('Error: "n_way" parameter is higher than the unique number of classes')
+    
+    def __len__(self):
+        return self.n_episodes
+    
+    def __iter__(self):
+        for batch in range(self.n_episodes):
+            batch = []
+            classes = torch.randperm(len(self.sample_indices))[:self.n_way]
+            for c in classes:
+                #l is a list of indexes of elements in target belonging to class c
+                l = self.sample_indices[c]
+                pos = torch.randperm(len(l))[:self.n_samples]
+                batch.append(l[pos])
+            batch = torch.stack(batch).t().reshape(-1)
+            yield batch
+        
